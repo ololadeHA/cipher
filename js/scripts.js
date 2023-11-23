0a21cf1b19747e1cc7b844db183cb1ce38f51efc
Arowolo Haleem <arowolohaleem@gmail.com>
2023-11-23 19:31:13 +0100

    add code to capitalize first and last letter to page

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@ const userSentence = prompt("Please enter a sentence")
 let text = userSentence
 
 //function to capitalize the first and last letter
-// function newString() {
-//   let value = text.charAt(0) + text.charAt(text.length - 1);
-//   value.toUpperCase();
-//   return value.toString(); 
+function newString() {
+  let value = text.charAt(0) + text.charAt(text.length - 1);
+  value.toUpperCase();
+  return value.toString(); 
 }
 
 //function to reverse  
